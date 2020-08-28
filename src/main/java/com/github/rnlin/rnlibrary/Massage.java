3cf8e083086a46aa7f99438acfdbf8b181acd5db
rnlin <43895616+rnlin430@users.noreply.github.com>
2020-08-28 23:34:28 +0900

    Update Massage.java

--- src/main/java/com/github/rnlin/rnlibrary/Massage.java
+++ src/main/java/com/github/rnlin/rnlibrary/Massage.java
@@ -1,3 +1,9 @@
+/**
+Region
+Copyright (c) 2020 rnlin
+This software is released under the MIT License.
+http://opensource.org/licenses/mit-license.php
+*/
 package com.github.rnlin.rnlibrary;
 
 import org.bukkit.entity.Player;
